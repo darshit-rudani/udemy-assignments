1eebf983821f44ab4d777366143e0bd342814694
Darshit Rudani <rdarshit111@gmail.com>
2021-07-31 19:36:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'My App',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
